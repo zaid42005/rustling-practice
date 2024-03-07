469ee2382f1673c3310b67aa3e46a0b984edbfe9
zaid42005 <zaid.42005@gmail.com>
2024-03-06 22:23:34 -0500

    done with move_semantics04

--- exercises/06_move_semantics/move_semantics4.rs
+++ exercises/06_move_semantics/move_semantics4.rs
@@ -11,7 +11,7 @@
 
 #[test]
 fn main() {
-    let vec0 = vec![22, 44, 66];
+    
 
     let mut vec1 = fill_vec();
 
@@ -21,9 +21,14 @@ fn main() {
 // `fill_vec()` no longer takes `vec: Vec<i32>` as argument - don't change this!
 fn fill_vec() -> Vec<i32> {
     // Instead, let's create and fill the Vec in here - how do you do that?
-    let mut vec = vec;
+    let mut vec: Vec<i32> = Vec::new();
 
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
+    
     vec.push(88);
+    
 
     vec
 }
